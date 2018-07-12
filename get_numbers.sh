2baa3daa3bf6b0f737b090c6fcba081976832b8e
rperi <rperi@usc.edu>
2018-07-11 23:53:11 -0700

    Commented and cleaned get_numbers.sh

--- get_numbers.sh
+++ get_numbers.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# This script computes the number of segments per each type of laughter
+# Input : 
+#       arg1 - directory containing segments
+#       arg2 - list of sessions as mrt files
+
+segments_dir=$1
+ses_list=$2
 
 laughter_only=0
 laughter_within=0
@@ -7,21 +14,21 @@ while=0
 
 while read -r line; do
 
-ses=$(basename $line '.mrt')
+    ses=$(basename $line '.mrt')
 
-num_laughter_only=$(wc -l ./laughter_segments/${ses}/laugh_only_segments.txt)
-num_laughter_within=$(wc -l ./laughter_segments/${ses}/laugh_utterance_segments.txt)
-num_breath=$(wc -l ./laughter_segments/${ses}/breath_laugh_segments.txt)
-num_while=$(wc -l ./laughter_segments/${ses}/while_laughing_segments.txt)
+    num_laughter_only=$(wc -l ${segments_dir}/${ses}/laugh_only_segments.txt)
+    num_laughter_within=$(wc -l ${segments_dir}/${ses}/laugh_utterance_segments.txt)
+    num_breath=$(wc -l ${segments_dir}/${ses}/breath_laugh_segments.txt)
+    num_while=$(wc -l ${segments_dir}/${ses}/while_laughing_segments.txt)
 
-laughter_only=$(( $laughter_only + $(echo $num_laughter_only | cut -d" " -f1)))
-laughter_within=$(( $laughter_within + $(echo $num_laughter_within | cut -d" " -f1)))
-breath=$(( $breath + $(echo $num_breath | cut -d" " -f1)))
-while=$(( $while + $(echo $num_while | cut -d" " -f1)))
+    laughter_only=$(( $laughter_only + $(echo $num_laughter_only | cut -d" " -f1)))
+    laughter_within=$(( $laughter_within + $(echo $num_laughter_within | cut -d" " -f1)))
+    breath=$(( $breath + $(echo $num_breath | cut -d" " -f1)))
+    while=$(( $while + $(echo $num_while | cut -d" " -f1)))
 
-done < ./transcripts/ses_list
+done < ${ses_list}
 
-echo $laughter_only
-echo $laughter_within
-echo $breath
-echo $while
+echo "Number of laughter only segments = "$laughter_only
+echo "Number of laughter within utterance segments = "$laughter_within
+echo "Number of breath laughter segments = "$breath
+echo "Number of laughter within utterance and overlapping with speech segments = "$while
