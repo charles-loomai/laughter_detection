a4666243f89f629a40b42e08b86b83c77a3f34f1
root <rperi@usc.edu>
2018-07-11 23:43:00 -0700

    added functionality to differentiate laugh only versus within utterance

--- get_laughter_stats.py
+++ get_laughter_stats.py
@@ -5,12 +5,13 @@ from collections import defaultdict
 
 """ This script takes as input an ICSI transcript file (.mrt) and returns the duration statistics of the 4 different kinds of laughters:
     1) laugh alone segments
-    2) laughing during uttering a word
+    2) laugh during utterance but NOT overlapping with speech
+    3) laughing during utterance overlapping with speech (while lauging)
     4) breath laugh
 
-    python get_laughter_stats.py <input_transcript>
+    python get_laughter_stats.py <input_transcript> <output_directory>
 
-    Returns a directory containing segments information of the different laughters 
+    Returns segments information of the different laughter types in output directory 
 """
 
 
@@ -37,17 +38,25 @@ def get_laughter_stats(inp_transcript, out_dir):
 
     bl_file.close()
 
-    # Write out timing information for laugh only segments
-    l_file = open("{0}/laugh_segments.txt".format(out_dir), 'w')
+    # Write out timing information for laugh only segments without any speech
+    l_only_file = open("{0}/laugh_only_segments.txt".format(out_dir), 'w')
+
+    # Write out timing information for laugh segments within utterance but not overlapping with word
+    l_file = open("{0}/laugh_utterance_segments.txt".format(out_dir), 'w')
+
     for seg in [bl for bl in xml_file.find_all('segment') if bl.find('vocalsound') and bl.has_attr('participant')]:
         if seg['participant'] in channel_dict.keys():
             if not seg.find('comment') or seg.find('comment')['description'] != 'Digits':
                 vocal_descriptions = [voc['description'] for voc in seg.find_all('vocalsound')]
                 for voc in vocal_descriptions:
                     if 'laugh' in voc and 'breath-laugh' not in voc:
-                        l_file.write("{0} {1} {2}\n".format(seg['starttime'], seg['endtime'], channel_dict[seg['participant']]))
+                        if not "{0}".format(seg.contents[0]).strip():  # The segment doesn't contain any speech. Only laughter
+                            l_only_file.write("{0} {1} {2}\n".format(seg['starttime'], seg['endtime'], channel_dict[seg['participant']]))
+                        else:
+                            l_file.write("{0} {1} {2}\n".format(seg['starttime'], seg['endtime'], channel_dict[seg['participant']]))
 
     l_file.close()
+    l_only_file.close()
 
     # Write out timing information for while-laughing segments
     wl_file = open("{0}/while_laughing_segments.txt".format(out_dir), 'w')
@@ -62,13 +71,12 @@ def get_laughter_stats(inp_transcript, out_dir):
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description='Extract duration statistics of various kinds of laughter')
-    parser.add_argument('inp_transcript', type=str, help="Input ICSI transcript file")
+    parser = argparse.ArgumentParser(description='Extract laughter segments per type of laughter')
+    parser.add_argument('inp_transcript', type=str, help="Input ICSI transcript file in mrt format")
     parser.add_argument('out_dir', type=str, help="Output directory for segments files")
 
     args = parser.parse_args()
 
     inp_transcript = args.inp_transcript
     out_dir = args.out_dir
-    #print(inp_transcript)
     get_laughter_stats(inp_transcript, out_dir)
